8ad2752af1aee3a2d2ea1744e9bd010eae9ac8b9
Jacopo Franzoi <jacopo.franzoi@gucci.com>
2022-01-05 16:33:41 +0100

    ci: debug info

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 
 function install_bats() {
     local os=$( uname -s | tr '[:upper:]' '[:lower:]' )
-    echo "OS: $os"
+    echo "OS: $os ($( uname -a ))"
     [[ "$os" == "darwin" ]] && brew install bats
     [[ "$os" == "linux" ]] && apk add bats --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/community/ --allow-untrusted
 }
